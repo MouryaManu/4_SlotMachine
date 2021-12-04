1b913bee5318f2e5a32ab1bbb1ddaba4906620b6
MouryaManu <67451638+MouryaManu@users.noreply.github.com>
2021-12-04 09:12:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+    render() {
+        return (
+          
+            <div>
+                <h1>Slot Machine!!</h1>
+                <Machine s1="🍕" s2="🍕" s3="🍕"  />
+                <Machine s1="🍕" s2="🍔" s3="🍟" />
+                <Machine s1="🍕" s2="🍔" s3="🍟" />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
